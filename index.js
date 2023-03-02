b87786175deff2337d255a2e0731c6f78230a325
tamzid620 <tamzid620@gmail.com>
2023-03-02 21:42:30 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+
+
+
+
+const loadHub = async() => {
+    const url = `https://openapi.programming-hero.com/api/ai/tools`
+    const res = await fetch(url);
+    const data = await res.json();
+    console.log(data);
+}
+
+loadHub();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
