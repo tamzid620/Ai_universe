630e1c46f5e6cd3b843555176f63c04905fc6d10
tamzid620 <tamzid620@gmail.com>
2023-03-03 00:38:38 +0600

    btn-dekho added

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const loadHubs = async() => {
     const url = `https://openapi.programming-hero.com/api/ai/tools`
     const res = await fetch(url);
     const data = await res.json();
-    displayHubs(data.data);
+    displayHubs(data.data.tools);
 }
 
 const displayHubs = hubs => {
@@ -15,17 +15,21 @@ const displayHubs = hubs => {
         const hubDiv = document.createElement('div');
         hubDiv.classList.add('col');
         hubDiv.innerHTML = `
-        <div class="card h-100">
-            <img src="..." class="card-img-top" alt="...">
+        <div class="card h-100 p-3"  style="height: 900px;">
+            <img src="${hub.image}" class="card-img-top" alt="...">
                 <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    <h5 class="card-title">Features</h5>
+                    <p class="card-text">${hub.features}</p>
                 </div>
         </div>
         `;
         hubsContainer.appendChild(hubDiv);
     })
 }
+
+
+
+
 
 loadHubs();
 
