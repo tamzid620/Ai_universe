aedf34cbf0fb3076c140183f81b51804f331823f
tamzid620 <tamzid620@gmail.com>
2023-03-02 22:41:02 +0600

    appendchild added

--- index.js
+++ index.js
@@ -2,14 +2,32 @@
 
 
 
-const loadHub = async() => {
+const loadHubs = async() => {
     const url = `https://openapi.programming-hero.com/api/ai/tools`
     const res = await fetch(url);
     const data = await res.json();
-    console.log(data);
+    displayHubs(data.data);
 }
 
-loadHub();
+const displayHubs = hubs => {
+    const hubsContainer = document.getElementById('hub-container')
+    hubs.forEach(hub =>{
+        const hubDiv = document.createElement('div');
+        hubDiv.classList.add('col');
+        hubDiv.innerHTML = `
+        <div class="card h-100">
+            <img src="..." class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                </div>
+        </div>
+        `;
+        hubsContainer.appendChild(hubDiv);
+    })
+}
+
+loadHubs();
 
 
 
