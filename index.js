3b5cf285372c3bffb32545ef52514b0fe30dfcb2
tamzid620 <tamzid620@gmail.com>
2023-03-03 08:32:27 +0600

    btn-motal added

--- index.js
+++ index.js
@@ -2,38 +2,63 @@
 
 
 
-const loadHubs = async() => {
+const loadHubs = async(dataLimit) => {
     const url = `https://openapi.programming-hero.com/api/ai/tools`
     const res = await fetch(url);
     const data = await res.json();
-    displayHubs(data.data.tools);
+    displayHubs(data.data.tools, dataLimit);
 }
 
-const displayHubs = hubs => {
+const displayHubs = (hubs,dataLimit)=> {
     const hubsContainer = document.getElementById('hub-container')
     // display 6 items only-------------
-    hubs = hubs.slice(0,6);
+    const seeMore = document.getElementById('btn-see-more');
+    if(hubs.length > 6 ){
+        hubs = hubs.slice(0,6);
+        seeMore.classList.remove('d-none');
+    }
+    else{
+        seeMore.classList.add('d-none');
+    }
+
     hubs.forEach(hub =>{
         const hubDiv = document.createElement('div');
         hubDiv.classList.add('col');
         hubDiv.innerHTML = `
-        <div class="card  p-3 shadow p-3  bg-body-tertiary rounded"  style="height: 550px;">
+        <div class="card  p-3 shadow p-3  bg-body-tertiary rounded text-start"  style="height: 500px;">
             <img src="${hub.image}" class="card-img-top" alt="...">
                 <div class="card-body">
                     <h5 class="card-title">Features</h5>
-                    <p class="card-text">${hub.features}</p>
+                    <p class="card-text text-secondary">${hub.features}</p>
+                    <hr>
+                    <div class="mt-5 d-flex justify-content-between">
+                        <div>
+                            <p class="card-text fw-bold">${hub.name}</p>
+                            <p class="card-text text-secondary">${hub.published_in}</p>
+                        </div>
+                        <div>
+                        <button id="btn-motal" class="button bg-info border border-info-subtle"><i class="fa-solid fa-hand-pointer"></i>  </button>
+                        </div>
+                    </div>
                 </div>
         </div>
         `;
         hubsContainer.appendChild(hubDiv);
     })
     toggleSpinner(false);
+    
+}
+
+const processSeach = (dataLimit) => {
+    loadHubs(dataLimit);
 }
 
 
-document.getElementById('btn-dekho').addEventListener('click', function(){
+document.getElementById('btn-see-more').addEventListener('click', function(){
     toggleSpinner(true);
+    processSeach();
 })
+
 const toggleSpinner = isLoading => {
     const spinnerSection = document.getElementById('spinner');
     if(isLoading){
@@ -41,6 +66,7 @@ const toggleSpinner = isLoading => {
     }else{
         loaderSection.classList.add('d-none');
     }
+
 }
 
 loadHubs();
